d451d22f35895c24f7699f0b74c70580e750640a
Abdulrhman Herzallah <abudherzallah0@gmail.com>
2020-05-07 13:13:58 +0300

    hel

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -9,6 +9,7 @@
     <script src="{{ asset('js/app.js') }}" defer></script>
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
     <!-- font Style -->
     <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@600&display=swap" rel="stylesheet">
